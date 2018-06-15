8e08c9c1001a0c4af81262bd44053e2b58413278
matya <matya@xFatex>
2018-06-14 21:49:01 -0400

    actualización de fibonacciService

--- src/main/java/com/matias/testrestservice/service/FibonacciService.java
+++ src/main/java/com/matias/testrestservice/service/FibonacciService.java
@@ -7,18 +7,19 @@ import com.matias.testrestservice.error.FibonacciException;
 @Service
 public class FibonacciService {
 	public Integer fibonacci(Integer n) throws FibonacciException{		
-	    if (n>1){
-	       return fibonacci(n-1) + fibonacci(n-2); 
-	    }
-	    else if (n==1) {  // caso base
+		if (n==0){  
+	        return 0;
+		}
+	    else 
+	    	if (n==1) {  
 	        return 1;
 	    }
-	    else if (n==0){  // caso base
-	        return 0;
-	    }
-	    else{ //error
-	        System.out.println("Debes ingresar un tamaño mayor o igual a 1");
-	        //return -1; 
+	    else 
+	    	if (n>1){
+		       return fibonacci(n-1) + fibonacci(n-2); 
+		    }
+	    else{ 
+	        System.out.println("Debes ingresar un tamaño mayor o igual a 1");	        
 	        throw new FibonacciException("Debes ingresar un tamaño mayor o igual a 1");
 	    }
 	}
