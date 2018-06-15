a1cd3ba5e9b2284173df9d25b4681ea322d1cbda
matya <matya@xFatex>
2018-06-15 11:00:32 -0400

    commit

--- src/test/java/com/matias/testrestservice2/FibonacciRestServiceApplicationTests.java
+++ src/test/java/com/matias/testrestservice2/FibonacciRestServiceApplicationTests.java
@@ -1,15 +1,33 @@
 package com.matias.testrestservice2;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.matias.testrestservice.controller.FibobacciController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FibonacciRestServiceApplicationTests {
+	
+	private MockMvc mockMvc;
+	private static final String URL = "/api/fibonacci/";
+	private final FibobacciController configurationController = new FibobacciController();
+	
 	@Test
 	public void contextLoads() {
+		
 	}
-
+	@Test
+	public void shouldReturnHttpCode200OnGet() throws Exception {
+		mockMvc.perform(get(URL).param("numero", "56").header("token", "2CX56733221DA21SQE11DTB5H")).andExpect(status().isOk());
+	}
+	
+	
 }
